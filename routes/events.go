44b433f6793fa069fc1c71678d1d5fa92c4f572c
Kamilya Kydykova <sultansakyp77@gmail.com>
2024-10-02 09:56:51 +0500

    routes package

---
+++ routes/events.go
@@ -0,0 +1,65 @@
+package routes
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/ssakyp/rest-api/models"
+)
+
+func getEvents(context *gin.Context) {
+	//the function sends back response
+
+	events, err := models.GetAllEvents()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch events. Try again later."})
+		return
+	}
+
+	// arguments for below function are status code and data (structs, maps and slices (gin.H{"message": "Hello!"}) to be sent
+	context.JSON(http.StatusOK, events) // HTML function can also be used but it is uncommon in REST API
+}
+
+// request handler
+func getEvent(context *gin.Context) {
+	// to get a path
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse event id"})
+		return
+	}
+
+	event, err := models.GetEventByID(eventId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch event. Try again later."})
+		return
+	}
+
+	context.JSON(http.StatusOK, event)
+
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event) //works like a fmt.Scan => stores the data into event => make sure that JSON corresponds our Event struct
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse requested data!"})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+
+	err = event.Save()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not create events. Try again later."})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
+}
---
+++ routes/routes.go
@@ -0,0 +1,12 @@
+package routes
+
+import "github.com/gin-gonic/gin"
+
+func RegisterRoutes(server *gin.Engine) {
+	//handler for GET request (GET, POST, PATCH, DELETE)
+	// if a "/events" is requested then the function (can be anonymous or named) will be executed
+	server.GET("/events", getEvents)
+
+	server.GET("/events/:id", getEvent) // /events/5, /events/1
+	server.POST("/events", createEvent) // in order to create a new event and should have some data for us
+}
